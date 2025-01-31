393cb8c3598fb7bf2b785cab8b1cd3b96a0938a8
Andrey Yarkovoy <ayarkovoy@nota.tech>
2025-02-01 01:02:00 +0200

    refactor: создана либа tt/auth

--- apps/tik-talk/src/app/app.routes.ts
+++ apps/tik-talk/src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { LayoutComponent } from './common-ui/layout/layout.component';
-import { canActivateAuth } from './auth/access.guard';
 import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 import { chatsRoutes } from './pages/chats-page/chatsRoutes';
 import { ExperimentFormComponent } from './pages/experiment-form/experiment-form.component';
+import { canActivateAuth } from '@tt/auth';
 
 export const routes: Routes = [
   {
--- libs/auth/src/lib/auth/access.guard.ts
+++ libs/auth/src/lib/auth/access.guard.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { AuthService } from './auth.service';
+import { AuthService } from '@tt/auth';
 import { Router } from '@angular/router';
 
 export const canActivateAuth = () => {
