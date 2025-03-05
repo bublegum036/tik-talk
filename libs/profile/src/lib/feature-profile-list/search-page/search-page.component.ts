e85712b04276cf08fd534290478c9509d478f0d7
Andrey Yarkovoy <ayarkovoy@nota.tech>
2025-03-05 11:04:05 +0200

    fix: исправлена верстка фильтров в поиске профилей

--- libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ProfileCardContainerComponent, ProfileFiltersComponent } from '../index';
+import { InfiniteScrollTriggerComponent, ProfileCardComponent } from '@tt/profile';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [ProfileFiltersComponent, ProfileCardContainerComponent],
+  imports: [ProfileFiltersComponent, ProfileCardContainerComponent, InfiniteScrollTriggerComponent, ProfileCardComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
